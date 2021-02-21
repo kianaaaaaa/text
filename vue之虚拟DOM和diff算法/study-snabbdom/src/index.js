65b4bdc6ac3491b0851e1ac5d1336bdc6b93856c
朝陇 <504473065@qq.com>
2021-02-21 16:23:43 +0800

    提交

--- vue之虚拟DOM和diff算法/study-snabbdom/src/index.js
+++ vue之虚拟DOM和diff算法/study-snabbdom/src/index.js
@@ -1,43 +1,5 @@
-import { init } from 'snabbdom/init'
-import { classModule } from 'snabbdom/modules/class'
-import { propsModule } from 'snabbdom/modules/props'
-import { styleModule } from 'snabbdom/modules/style'
-import { eventListenersModule } from 'snabbdom/modules/eventlisteners'
-import { h } from 'snabbdom/h' // 用于创建vnode的辅助函数
+import h from '../src/mysnabbdom/h'
 
-const patch = init([ // 带有选定模块的初始化补丁功能
-    classModule, // makes it easy to toggle classes
-    propsModule, // for setting properties on DOM elements
-    styleModule, // handles styling on elements with support for animations
-    eventListenersModule, // attaches event listeners
-])
 
-const container = document.getElementById('container')
-
-const vnode = h('div#container.two.classes', {
-    on: {
-        click: function () {
-
-        }
-    }
-}, [
-    h('span', { style: { fontWeight: 'bold' } }, 'This is bold'),
-    ' and this is just normal text',
-    h('a', { props: { href: '/foo' } }, 'I\'ll take you places!')
-])
-//修补到空的DOM元素–这将DOM修改为副作用
-patch(container, vnode)
-
-const newVnode = h('div#container.two.classes', {
-    on: {
-        click: function () {
-
-        }
-    }
-}, [
-    h('span', { style: { fontWeight: 'normal', fontStyle: 'italic' } }, 'This is now italic type'),
-    ' and this is still just normal text',
-    h('a', { props: { href: '/bar' } }, 'I\'ll take you places!')
-])
-// Second `patch` invocation
-patch(vnode, newVnode) // Snabbdom efficiently updates the old view to the new state
+const nodes = h ('div',{},[])
+console.log(nodes)
