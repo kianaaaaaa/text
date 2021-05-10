7c68e738ef4c1ed8c1c2341dfa7c52c7c07598c4
zc <504473065@qq.com>
2021-05-10 14:51:20 +0800

    页面完善

--- 仿知乎专栏vue3-ts/zhihu/src/components/ColumnList.vue
+++ 仿知乎专栏vue3-ts/zhihu/src/components/ColumnList.vue
@@ -1,11 +1,63 @@
 <template>
-  <h1>list</h1>
+  <el-row :gutter="20">
+    <el-col :span="8" v-for="column in list" :key="column.id" class="el-row--flex">
+      <el-card :body-style="{ padding: '10px' }">
+        <img :src="column.active" :alt="column.title" class="img">
+        <div style="padding: 14px;">
+          <span>{{ column.title }}</span>
+          <div class="bottom">
+            <time class="time">{{ column.description }}</time>
+            <el-button type="text" class="button">进入专栏</el-button>
+          </div>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script lang="ts" setup>
+import { defineProps, PropType } from 'vue'
 
+export interface ColumnProps {
+  id: number,
+  title: string,
+  active: string,
+  description: string
+}
+
+defineProps({
+  list: {
+    type: Array as PropType<ColumnProps[]>,
+    require: true
+  }
+})
 </script>
 
 <style scoped>
+.time {
+  font-size: 13px;
+  color: #999;
+}
 
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.el-row--flex {
+  margin-bottom: 20px;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
 </style>
--- 仿知乎专栏vue3-ts/zhihu/src/views/Home.vue
+++ 仿知乎专栏vue3-ts/zhihu/src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+
+  <Column-list :list='list'></Column-list>
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script lang="ts" setup>
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue' // @ is an alias to /src
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test专栏1',
+    description: '这是test专栏1,有一段非常有意思的简介,请填一下吧!',
+    active: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }, {
+    id: 2,
+    title: 'test专栏2',
+    description: '这是test专栏2,有一段非常有意思的简介,请填一下吧!',
+    active: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }, {
+    id: 3,
+    title: 'test专栏3',
+    description: '这是test专栏3,有一段非常有意思的简介,请填一下吧!',
+    active: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }, {
+    id: 4,
+    title: 'test专栏4',
+    description: '这是test专栏4,有一段非常有意思的简介,请填一下吧!',
+    active: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
-})
+]
+const list = testData
+
 </script>
