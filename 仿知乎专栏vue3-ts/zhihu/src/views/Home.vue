40705fed8cfb6ce49fde0f540e5e1e3b380ff954
zc <504473065@qq.com>
2021-05-27 17:25:36 +0800

    更新 Home.vue

--- 仿知乎专栏vue3-ts/zhihu/src/views/Home.vue
+++ 仿知乎专栏vue3-ts/zhihu/src/views/Home.vue
@@ -3,7 +3,7 @@
     <section class="py-5 text-center container">
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
-          <el-image lazy src="http://fxjc.cgbchina.com.cn/images/pc_kv.png" alt="callout" class="w-50"/>
+          <el-image lazy src="http://fxjc.cgbchina.com.cn/images/pc_kv.png" alt="callout" class="w-50" />
           <h2 class="font-weight-light">随心写作，自由表达</h2>
           <p>
             <a href="#" class="btn btn-primary my-2">开始写文章</a>
@@ -18,16 +18,15 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, computed } from 'vue'
-import { useStore } from 'vuex'
-import { GlobalDataProps } from '../store/index'
-import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { ref, onMounted, computed } from "vue";
+import { useStore } from "vuex";
+import { GlobalDataProps } from '@/store';
+import ColumnList, { ColumnProps } from "@/components/ColumnList.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const store = useStore<GlobalDataProps>()
-const list = computed(() => store.state.columns)
-const lenth = computed(() => store.getters.biggerColumnsLen)
+const store = useStore<GlobalDataProps>();
+const list = computed(() => store.state.columns);
+const lenth = computed(() => store.getters.biggerColumnsLen);
 </script>
 <style>
-
 </style>
