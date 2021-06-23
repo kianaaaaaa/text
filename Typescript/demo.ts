610434d1ba14c00b6236cec3ca7300b3d5a3bec4
Kiana ✨ <504473065@qq.com>
2021-06-23 18:34:43 +0800

    ts

--- Typescript/demo.ts
+++ Typescript/demo.ts
@@ -1,4 +1,24 @@
-const getName = (name, age) => {
-    console.log(name)
-    console.log(age)
+interface age {
+    name: string,
+    say: () => {}
 }
+
+interface nn {
+    name: string,
+    anion: () => {}
+}
+const obj = {
+    name:'王五',
+    anion:()=>{
+        console.log('啊啊啊')
+    }
+}
+function ask(anime: age | nn) {
+    if (anime.name === "李四") {
+        (anime as age).say()
+    } else {
+        (anime as nn).anion()
+    }
+}
+
+console.log('123')
